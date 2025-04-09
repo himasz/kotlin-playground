c9840032422cae50ad8100d1b67ba93ea6a4ab6b
ebrahimzidan <himaszidan@gmail.com>
2025-04-09 23:13:51 +0200

    clean up

--- src/main/kotlin/com/example/TaskManagerApplication.kt
+++ src/main/kotlin/com/example/TaskManagerApplication.kt
@@ -2,10 +2,8 @@ package com.example
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["com.example"])
 open class TaskManagerApplication
 
 fun main(args: Array<String>) {
